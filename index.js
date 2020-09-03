e352c48c7cc27e3e5540e800749e82ef159ade86
Jake <jtschwantes@gmail.com>
2020-09-03 13:07:32 -0600

    connected to database

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL || 'postgres://yfyzdciabhwwbq:d3a9d00ce2cd06349f5ac0ce0139b6467aee5d3994881f85faa0ad5d35c7abac@ec2-52-72-34-184.compute-1.amazonaws.com:5432/daq4b6l917ll6r',
+    ssl: true
+});
+
 const PORT = process.env.PORT || 5000
+
 
 const app = express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,4 +15,20 @@ const app = express()
   .set('view engine', 'ejs')
 //   .get('/', (req, res) => res.render('pages/index'))
   .get('/', (req, res) => res.send('Hello World!'))
+  .get('/projects', getProjects)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  async function getProjects(req, res) {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM projects');
+        // const results = { 'results': (result) ? result.rows : null };
+        const results = result.rows;
+        //   res.render('pages/db', results );
+        res.send(results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
