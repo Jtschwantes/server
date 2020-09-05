71af37ef7b4573706879b23739de73436de5703d
Jake <jtschwantes@gmail.com>
2020-09-05 16:00:57 -0600

    allowed cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bp = require('body-parser')
 const path = require('path')
-const { Pool } = require('pg');
+const cors = require('cors')
+const { Pool } = require('pg')
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || 'postgres://yfyzdciabhwwbq:d3a9d00ce2cd06349f5ac0ce0139b6467aee5d3994881f85faa0ad5d35c7abac@ec2-52-72-34-184.compute-1.amazonaws.com:5432/daq4b6l917ll6r',
     ssl: { rejectUnauthorized: false }
@@ -81,6 +82,7 @@ const app = express()
     // Set Up options
     .use(express.static(path.join(__dirname, 'public')))
     .use(bp.json())
+    .use(cors())
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     // Project services
